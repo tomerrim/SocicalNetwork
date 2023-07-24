6e3d322a437a5a8449996e663ca6f90b40857f88
Tomer <tomerrim@gmail.com>
2023-07-24 23:40:26 +0300

    added metadata to all of the pages, fetch posts data from dummyjson, added loading and error and added Post interface

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { getPost } from "@/services/posts";
 
 interface PostView {
     params: {
@@ -6,17 +7,30 @@ interface PostView {
     }
 }
 
-export default function PostViewPage(props: PostView) {
+export function generateMetadata(props: PostView) {
+    return {
+        title: `Post ${props.params.postId}`,
+        description: `Post ${props.params.postId}`,
+    }
+}
+
+export function generateStaticParams() {
+    return [{postId: "2"}, {postId: "4"}, {postId: "12"},]
+}
+
+export default async function PostViewPage(props: PostView) {
     const { postId } = props.params;
+    const {title, body} = await getPost(postId);
     return (
         <>
             <header className="flex items-center mb-6">
-                <h1>PostView {postId} Page</h1>
+                <h1>{title}</h1>
                 <div className="ml-auto">
                     <Link className="btn" href={`/posts/${postId}/edit`}>Edit</Link>
                     <button className="btnRed">Delete</button>
                 </div>
             </header>
+            <p>{body}</p>
         </>
     )
 }
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,21 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import Post from "@/types/Post";
 
-interface PostCardProps {
-    postId: string;
-
-}
-
-export default function PostCard(props: PostCardProps) {
+export default function PostCard(props: Post) {
+    const { id: postId, title, body } = props;
+    const titleForImage = title.replace(" ", "").slice(0, 2).toUpperCase();
     return (
         <>
             <li className="rounded-lg shadow-lg bg-neutral-700">
-                <Image className="rounded-t-lg" src={`https://via.placeholder.com/600x400?text=Post ${props.postId}`} width="600" height="400" alt={`post ${props.postId}`}/>
-                <Link href={`/posts/${props.postId}`}>
+                <Image className="rounded-t-lg" src={`https://via.placeholder.com/600x400?text=${titleForImage}`} width="600" height="400" alt={title}/>
+                <Link href={`/posts/${postId}`}>
                     <div className="p-4">
-                        <h4 className="text-xl text-neutral-50 font-medium">Title</h4>
-                        <p className="text-neutral-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, corporis. Perspiciatis optio magnam, 
-                            unde eligendi laborum quo ut delectus aperiam porro! Sint fugiat itaque, eveniet accusamus nostrum facilis tenetur asperiores.</p>
+                        <h4 className="text-xl text-neutral-50 font-medium">{title}</h4>
+                        <p className="text-neutral-50">{body.slice(0,50)}</p>
                     </div>
                 </Link>
             </li>
