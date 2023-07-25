1b5035efb593d5c73f6d98fb368a3d246a7c1496
Tomer <tomerrim@gmail.com>
2023-07-25 13:29:41 +0300

    added new cache on demand revalidation

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -8,7 +8,6 @@ interface PostView {
     }
 }
 export const dynamic = "force-static";
-export const revalidate = 3;
 
 export function generateMetadata(props: PostView) {
     return {
---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,20 @@
+// api/revalidate
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler( request: NextApiRequest, response: NextApiResponse ) {
+    if(request.method === "POST") {
+        const { secret, paths } = request.body;
+        if(secret !== process.env.REVALIDATE_SECRET) {
+            return response.status(401).json({ message: "Invalid Secret" });
+        }
+        
+        try {
+            await Promise.all(paths.map(response.revalidate));
+            return response.json({ message: "Revalidated successfully" });
+        } catch (error) {
+            console.error(error);
+            return response.status(500).json({ message: "Internal Server Error"});
+        }
+    }
+}
